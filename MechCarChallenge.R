af3841e264b8c205baa50d779837511a6c857615
Kevin Blum <gkmb23@Gmail.com>
2022-04-01 16:33:06 -0500

    REadMe updates

--- MechCarChallenge.R
+++ MechCarChallenge.R
@@ -1,14 +1,33 @@
 #Imports Tinyverse & Dyplr Library
 library(tinyverse)
+
 #Imports the MechaCar MPG CSV file as dataframe
 MechaCarMPGdf <- read.csv(file = "MechaCar_mpg.csv")
+
 #Perform Multiple Linear Regression and pass data into the previously created DF
 lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data = MechaCarMPGdf)
+
 #Summary of the Multiple Linear Regression from previous step
 summary(lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data = MechaCarMPGdf))
+
 #Import Suspension Coil Data from CSV file as DataFrame
 Suspension_Data <- read.csv(file = "Suspension_Coil.csv")
+
 #Create Total Summary Object - with Mean Median Variance and Standar Dev of PSI
 total_summary <- Suspension_Data %>% summarize (Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
+
 #Create new Summary Object that organizes previous data by Lot #
 lot_summary <- Suspension_Data %>% group_by(Manufacturing_Lot) %>%  summarize (Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
+
+#Run a t Test to determine if the PSI is statistically different across lots  
+PSI_data_sample <- Suspension_Data %>% filter(PSI>1) %>% sample_n(30)
+t.test(PSI_data_sample$PSI,Suspension_Data$PSI)
+
+
+#Run a t Test to determine if the PSI is statistically different across each of the lots individually
+sample_lot <- Suspension_Data %>%  filter(Manufacturing_Lot== 'Lot1') %>% sample_n(10)
+sample_lot2 <- Suspension_Data %>%  filter(Manufacturing_Lot== 'Lot2') %>% sample_n(10)
+sample_lot3 <- Suspension_Data %>%  filter(Manufacturing_Lot== 'Lot3') %>% sample_n(10)
+t.test(sample_lot$PSI,sample_lot2$PSI)
+t.test(sample_lot$PSI,sample_lot3$PSI)
+t.test(sample_lot2$PSI,sample_lot3$PSI)
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # MechaCar_Statistical_Analysis
+
+## Linear Regression to Predict MPG
+
+Reviewing the data image below; you will notice that the P-Values for Ground Clearance and MPG categories were sufficient below the standard P-Value of '.05'.  Given that they scored below the P-Value we can safely say that they would meet the criteria to provide non-random variance to performance.  While the remaining categories all scored above the average P-value indicating that they would fail to reject the null hypothesis - therefore providing random variance levels to performance.
+
+
+
+![MultipleLinearRegression]
